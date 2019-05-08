6e9f9a2ecb8294b830e64cb4a2180b92b4dbc109
Szymon Sentkowski <szysen123@gmail.com>
2019-05-09 01:22:49 +0200

    Added microinteractions

--- scripts.js
+++ scripts.js
@@ -4,3 +4,37 @@ window.onscroll = function(ev) {
         document.querySelector(".footer-flower-left").style.animationPlayState = 'running';
     }
 };
+
+const tourButton = document.querySelector(".tour-show-more-button");
+tourButton.addEventListener("click", toggleTour);
+
+const singleButton = document.querySelectorAll(".single-button");
+singleButton.forEach((button) => {
+    button.addEventListener("click", toggleSingleSelection)
+})
+
+function toggleTour() {
+    const tourButton = document.querySelector(".tour-show-more-button");
+    const tourDates = document.querySelectorAll(".see-more-tour");
+    if (tourDates[0].style.display === 'none' || !tourDates[0].style.display) {
+        tourDates.forEach(tour => {
+            tour.style.display = 'block';
+        });
+        tourButton.textContent = 'Collapse'
+    } else {
+        tourDates.forEach(tour => {
+            tour.style.display = 'none'
+        })
+        tourButton.textContent = 'See more'
+    }
+}
+
+function toggleSingleSelection(evt) {
+    const selectionContainer = evt.target.nextElementSibling;
+    selectionContainer.style.display = 'block';
+    document.addEventListener("click", (outerEvt) => {
+        if (outerEvt.target !== evt.target && outerEvt.target !== selectionContainer) {
+            selectionContainer.style.display = 'none';
+        }
+    })
+}
